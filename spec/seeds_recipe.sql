8b641acb10904ea819573428c9e4b2614e103917
Dora Gores <d.lopesgores@gmail.com>
2023-02-05 11:53:15 +0000

    Adding recipes design md files

---
+++ spec/seeds_recipe.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE recipes RESTART IDENTITY;
+
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Burrito', '60 minutes', '5');
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Carbonara', '30 minutes', '3');
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Chicken Strogonoff', '40 minutes', '5');
